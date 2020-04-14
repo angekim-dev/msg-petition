91c6cb648f71822fd3786f6ea1f63104b2238afb
ANGELIKA KIM <alika852@gmail.com>
2020-04-14 18:08:24 +0200

    updated script.js with Alistair's help - still invisible signature :(

--- public/script.js
+++ public/script.js
@@ -3,12 +3,12 @@
     console.log("script.js running");
 
     const canvas = document.getElementById("canvas");
-    console.log(canvas);
+    // console.log(canvas);
     const canCtx = canvas.getContext("2d");
-    console.log(canCtx);
+    // console.log(canCtx);
     const offset = canvas.getBoundingClientRect();
     let dataURL;
-    // let hiddenInput = document.getElementById("hiddenInput");
+    let box = document.getElementById("hiddenInput");
 
     let drawing = false;
     let x = 0;
@@ -28,9 +28,9 @@
             y = e.clientY - offset.top;
             canCtx.lineTo(x, y);
             canCtx.stroke();
-            dataURL = $("#canvas")[0].toDataURL();
-            $("#hiddenInput").val(dataURL);
-            console.log($("#hiddenInput").val(dataURL));
+            dataURL = canvas.toDataURL();
+            box.value = dataURL;
+            console.log(box.value);
         }
     });
 
@@ -40,6 +40,6 @@
             y = 0;
             drawing = false;
         }
-        console.log(dataURL);
+        // console.log(dataURL);
     });
 })();
